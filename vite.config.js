fe392b167e64db71a16d2ba95b77f33f83ba498a
Arnold Wilson Fernandes <arnoldfern.1945@gmail.com>
2026-07-26 00:42:16 +0530

    Fixing the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-export default {
-  // Set the base directory for GitHub pages
-  base: '/GridVille/',
+import { defineConfig } from 'vite';
 
-  // Set the project root directory (relative to the config file)
+export default defineConfig({
+
+  base: process.env.NODE_ENV === 'production' ? '/GridVille/' : '/',
+
   root: './src',
 
-  // Set the directory to serve static files from (relative to the root)
   publicDir: './public',
   
-  // Set the build output directory
   build: {
-    outDir: './dist'
+    outDir: '../dist',
+    emptyOutDir: true
   }
-}
+});
